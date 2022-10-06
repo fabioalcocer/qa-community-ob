f7f30d3163d5cfea189dbe07a828ef01b569ef74
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-10-06 18:21:11 -0400

    fix header height

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png'
 
 function Header () {
   return (
-    <header className='w-full px-5 bg-slate-800 h-20 rounded-b-lg'>
+    <header className='w-full px-5 bg-slate-800 h-24 rounded-b-lg'>
       <nav className='max-w-7xl h-full container mx-auto flex items-center justify-between'>
         <img src={logo} alt='logo' className='w-10 h-10 md:w-14 md:h-14' />
         <h1 className='text-xl md:text-2xl text-slate-100'>Q&A OpenBootcamp</h1>
